e4b59568692a463a02bf5f6861f9d28523d2456a
Daniel Bazaco <danielbazacovelasco@gmail.com>
2022-01-02 22:04:37 +0100

    Added + conversion.

--- src/engine/conversions.js
+++ src/engine/conversions.js
@@ -1,5 +1,5 @@
 import { AtomicPattern } from "../grammar/ast";
-import { ASTERISK } from "../grammar/astBuilder";
+import { ASTERISK, PLUS } from "../grammar/astBuilder";
 import { CharacterMatcher, EPSILON, NFA } from "./dfa";
 
 let i = 0;
@@ -18,22 +18,19 @@ export function regexToNFA(regexAST) {
     let nfa = null;
     resetStateNumbers();
     for (const c of regexAST.subpatterns) {
-        let base;
+        let baseBuilder, base;
         if (c.child instanceof AtomicPattern) {
-            base = atomicPatternNFA(c.child.char);
+            baseBuilder = () => atomicPatternNFA(c.child.char);
         }
 
         if (c.quantifier === ASTERISK) {
-            const newInit = newState();
-            const newEnd = newState();
-            base.addState(newInit); base.addState(newEnd);
-            base.addTransition(newInit, base.initialState, new CharacterMatcher(EPSILON));
-            // 0 because it can only have a single ending state (with this construction)
-            base.addTransition(base.endingStates[0], newEnd, new CharacterMatcher(EPSILON));
-            base.addTransition(base.endingStates[0], base.initialState, new CharacterMatcher(EPSILON));
-            base.addTransition(newInit, newEnd, new CharacterMatcher(EPSILON));
-            base.setInitialState(newInit);
-            base.setEndingStates([newEnd]);
+            base = asterisk(baseBuilder);
+        } else if (c.quantifier === PLUS) {
+            base = baseBuilder();
+            const extraPart = asterisk(baseBuilder);
+            base.thompsonAppendNFA(extraPart, base.endingStates[0]);
+        } else {
+            base = baseBuilder();
         }
         if (nfa === null) 
             nfa = base 
@@ -41,6 +38,21 @@ export function regexToNFA(regexAST) {
             nfa.thompsonAppendNFA(base, nfa.endingStates[0]);
     }
     return nfa;
+}
+
+function asterisk(builder) {
+    const newInit = newState();
+    const base = builder();
+    const newEnd = newState();
+    base.addState(newInit); base.addState(newEnd);
+    base.addTransition(newInit, base.initialState, new CharacterMatcher(EPSILON));
+    // 0 because it can only have a single ending state (with this construction)
+    base.addTransition(base.endingStates[0], newEnd, new CharacterMatcher(EPSILON));
+    base.addTransition(base.endingStates[0], base.initialState, new CharacterMatcher(EPSILON));
+    base.addTransition(newInit, newEnd, new CharacterMatcher(EPSILON));
+    base.setInitialState(newInit);
+    base.setEndingStates([newEnd]);
+    return base;
 }
 
 function atomicPatternNFA(character) {
